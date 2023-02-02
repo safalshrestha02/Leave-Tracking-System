a3737b2e188c17b0d865527a7962c530aea2f647
shresthasafal1 <shresthasafal000@gmail.com>
2023-02-02 19:37:05 +0545

    feat: rendered html pages.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const app = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+//routes
+//const connectDB = require("./config/connect");
+const loginPage = require("./routes/loadPages");
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// connectDB();
+
+app.get("/", loginPage);
+app.get("/home", loginPage);
+app.get("/leave", loginPage);
+
+// mongoose.connection.once("open", () => {
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+// });
