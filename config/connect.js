1d7f11f8111f3b1e0d0cdb2852cfaa60845de96a
shresthasafal1 <shresthasafal000@gmail.com>
2023-02-02 22:44:02 +0545

    feat: rendered pages from BE.

---
+++ config/connect.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+
+// exports.connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DBURI),
+//       {
+//         newUrlParse: true,
+//         useUnifiedTopology: true,
+//       };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
