5e5baf1baa8fd8b99bfbea486e0929bff2822745
Trishan Wagle <mailtotrishan@gmail.com>
2023-02-14 17:59:10 +0545

    feat: fetched client profile details from api.

--- public/scripts/client/client_dashboard.js
+++ public/scripts/client/client_dashboard.js
@@ -1,17 +1,14 @@
-let addWorker  = document.getElementById("button-add-worker")
-let formContainer = document.querySelector(".register-container")
-let formClose = document.getElementById("close-form")
+const addWorker = document.getElementById("button-add-worker")
+const formContainer = document.querySelector(".register-container")
+const formClose = document.getElementById("close-form")
 
 addWorker.addEventListener("click", () => {
     console.log("hello")
     formContainer.classList.add("form-active");
     console.log("hello")
-    
+
 })
 
-formClose.addEventListener("click",() => {
+formClose.addEventListener("click", () => {
     formContainer.classList.remove("form-active");
 })
-
-
-
