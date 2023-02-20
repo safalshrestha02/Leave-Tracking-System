c99f914d9e50deffaae69a7e9a8d424e63f27a95
Magiciaan <surajkhadka315@gmail.com>
2023-02-20 19:08:47 +0545

    fixed worker api

--- controller/tempPage.js
+++ controller/tempPage.js
@@ -20,7 +20,7 @@ exports.apiWorkers = async (req, res, next) => {
   // worker.find().then(async (result) =>  {
   const Cworker = await worker
     .find({})
-    .populate ( ClientRegistration,{ path: "companyN", model: "client" });
+    // .populate ( ClientRegistration,{ path: "companyN", model: "client" });
   res.json(Cworker);
 
   // res.send(result);
