933df480ebe0ed8da231bb567c9ea3d9da7f3e14
ishanghimire11 <ishanuchiha11@gmail.com>
2023-02-20 17:01:10 +0545

    feat: search functionality on client manage workers

--- public/scripts/client/client_manage_worker.js
+++ public/scripts/client/client_manage_worker.js
@@ -1,11 +1,78 @@
-const manageWorkersSection = document.querySelector('.manage-workers-leave')
+const manageWorkersSection = document.querySelector(".manage-workers-leave");
+const dashboardSearch = document.querySelector(".dashboard-search");
 
 const fetchWorkers = async () => {
-    const workersData = await fetchWorkersApi()
-    workersData.map((data) => {
-        const { firstName, lastName, workerID, gender, email } = data
-        const fullName = `${firstName} ${lastName}`
-        let ihtml = `
+  const workersData = await fetchWorkersApi();
+
+  //-------------------- Confirm delete function ------------------------------//
+
+  const confirmDelete = () => {
+    const deleteButtons = document.querySelectorAll(".worker-delete-button");
+    const confirmBox = document.querySelector(".confirm-container");
+    const cancelButton = document.querySelector(".cancel-button");
+
+    deleteButtons.forEach((deleteButton) => {
+      deleteButton.addEventListener("click", () => {
+        confirmBox.style.display = "flex";
+      });
+      cancelButton.addEventListener("click", () => {
+        confirmBox.style.display = "none";
+      });
+    });
+  };
+
+  // ----------------------- search Function ------------------------------------//
+
+  const searchFunc = () => {
+    const filtered = workersData.filter((workerInfo) => {
+      return workerInfo.firstName.startsWith(dashboardSearch.value);
+    });
+    manageWorkersSection.innerHTML = "";
+    filtered.map((data) => {
+      const { firstName, lastName, workerID, gender, email } = data;
+      const fullName = `${firstName} ${lastName}`;
+      let ihtml = `
+            <div class="worker-details">
+
+                <div class="topsection">
+                
+                    <span>
+                        <i class="fa-regular fa-user user-icon"></i>
+                        <span class="worker-name">${fullName}</span>
+                    </span>
+                    <a href="#" class="worker-profile">
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </a>
+
+                </div>
+
+
+                <div class="worker-id">
+                    <span>Worker ID: ${workerID}</span>
+                </div>
+
+                <div class="worker-gender-delete">
+                    <p class="gender">${gender}</p>
+
+                    <button class="worker-delete-button">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+
+                </div>
+
+            </div>
+            `;
+      manageWorkersSection.innerHTML += ihtml;
+      confirmDelete();
+    });
+  };
+
+  dashboardSearch.addEventListener("input", searchFunc);
+  manageWorkersSection.innerHTML = "";
+  workersData.map((data) => {
+    const { firstName, lastName, workerID, gender, email } = data;
+    const fullName = `${firstName} ${lastName}`;
+    let ihtml = `
         <div class="worker-details">
 
             <div class="topsection">
@@ -36,8 +103,10 @@ const fetchWorkers = async () => {
             </div>
 
         </div>
-        `
-        manageWorkersSection.insertAdjacentHTML('afterbegin', ihtml)
-    })
-}
-fetchWorkers()
+        `;
+    manageWorkersSection.innerHTML += ihtml;
+    confirmDelete();
+  });
+};
+
+fetchWorkers();
