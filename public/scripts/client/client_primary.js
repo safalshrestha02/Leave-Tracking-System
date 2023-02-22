a5f438a8ab0ceb13356918393cab8cd685f0e562
Trishan Wagle <mailtotrishan@gmail.com>
2023-02-22 17:50:20 +0545

    feat: mapped leave requests according to new format of api.

--- public/scripts/client/client_primary.js
+++ public/scripts/client/client_primary.js
@@ -1,44 +1,59 @@
-const clientsApiUrl = "http://localhost:3000/api/clients";
+const clientsApiUrl = 'http://localhost:3000/api/clients'
 const fetchClientsApi = async () => {
-  try {
-    const response = await fetch(clientsApiUrl);
-    const result = await response.json();
-    const {
-      companyName,
-      companyAddress,
-      name: clientName,
-      email: clientEmail,
-      _id,
-    } = result[0];
-    return { companyName, companyAddress, clientName, clientEmail, _id };
-  } catch (err) {
-    console.log(err);
-  }
-};
+    try {
+        const response = await fetch(clientsApiUrl)
+        const result = await response.json()
+        const { companyName, companyAddress, name: clientName, email: clientEmail, _id } = result[0]
+        return { companyName, companyAddress, clientName, clientEmail, _id }
+    }
 
-const workersApiUrl = "http://localhost:3000/api/workers";
+    catch (err) {
+        console.log(err)
+    }
+}
+
+
+const workersApiUrl = 'http://localhost:3000/api/workers'
 const fetchWorkersApi = async () => {
-  try {
-    const response = await fetch(workersApiUrl);
-    const workersData = await response.json();
-    return workersData;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+    try {
+        const response = await fetch(workersApiUrl)
+        const workersData = await response.json()
+        return workersData
+    }
 
-const leaveRequestsApiUrl = "http://localhost:3000/api/leaveRequests";
+    catch (err) {
+        console.log(err.message)
+    }
+}
+
+
+const leaveRequestsApiUrl = 'http://localhost:3000/api/leaveRequests'
 const fetchLeaveRequestsApi = async () => {
-  const response = await fetch(leaveRequestsApiUrl);
-  const leaveRequestsData = await response.json();
-  return leaveRequestsData;
-};
+    const response = await fetch(leaveRequestsApiUrl)
+    const leaveRequestsData = await response.json()
+    return leaveRequestsData
 
+}
+
+
+// Getting the actual workers
 const workersUnderClient = async () => {
-  const activeClient = await fetchClientsApi();
-  const totalWOrkers = await fetchWorkersApi();
-  const workersClient = totalWOrkers.filter((worker) => {
-    return worker.companyName === activeClient.companyName;
-  });
-  return workersClient;
-};
+    const activeClient = await fetchClientsApi();
+    const totalWorkers = await fetchWorkersApi();
+    const workersClient = totalWorkers.filter((worker) => {
+        return worker.companyName === activeClient.companyName;
+    })
+    return workersClient;
+}
+
+
+// Getting the actual leave requests
+const leaveRequestsUnderClient = async () => {
+    const activeClient = await fetchClientsApi();
+    const totalLeaveRequests = await fetchLeaveRequestsApi()
+    const leaveRequestsClient = totalLeaveRequests.filter((leaveRequest) => {
+        const { workerDetails } = leaveRequest
+        return workerDetails.companyName === activeClient.companyName
+    })
+    return leaveRequestsClient
+}
