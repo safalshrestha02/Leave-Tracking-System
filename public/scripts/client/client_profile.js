0eae74093e81ba92801df85cd117af1db7047c8f
Trishan Wagle <mailtotrishan@gmail.com>
2023-02-23 14:54:02 +0545

    ss

--- public/scripts/client/client_profile.js
+++ public/scripts/client/client_profile.js
@@ -2,22 +2,23 @@ const dashboard = document.querySelector(".dashboard-content");
 const dashboardClientName = document.querySelector(".client-name");
 const dashboardClientEmail = document.querySelector(".client-email-value");
 const dashboardCompanyName = document.querySelector(".client-company-value");
-const dashboardCompanyAddress = document.querySelector(
-  ".client-company_address-value"
-);
+const dashboardCompanyAddress = document.querySelector(".client-company_address-value");
 const dashboardWorkers = document.querySelector(".client-workers-value");
 const dashboardClientCountry = document.querySelector(".client-country-value");
 const dashboardClientCity = document.querySelector(".client-city-value");
+const companyNameFieldProfile = document.querySelector('#company-name');
+
 
 const fetchClientProfile = async () => {
   try {
-    const { companyName, companyAddress, clientName, clientEmail } =
-      await fetchClientsApi();
+    const totalWorkers = await workersUnderClient()
+    const { companyName, companyAddress, clientName, clientEmail } = await fetchClientsApi();
     dashboardClientName.innerHTML = clientName;
     dashboardClientEmail.innerHTML = clientEmail;
     dashboardCompanyName.innerHTML = companyName;
+    companyNameFieldProfile.innerHTML = companyName;
     dashboardCompanyAddress.innerHTML = companyAddress;
-    dashboardWorkers.innerHTML = 300;
+    dashboardWorkers.innerHTML = totalWorkers.length;
     dashboardClientCountry.innerHTML = "Nepal";
     dashboardClientCity.innerHTML = "Kathmandu";
   } catch (err) {
