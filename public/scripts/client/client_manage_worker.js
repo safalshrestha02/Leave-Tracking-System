9dc98c8f84908ede0c15e1eb6ea675ec48912921
ishanghimire11 <ishanuchiha11@gmail.com>
2023-02-24 19:40:09 +0545

    feat: added sorting to manage workers

--- public/scripts/client/client_manage_worker.js
+++ public/scripts/client/client_manage_worker.js
@@ -1,6 +1,8 @@
 const manageWorkersSection = document.querySelector(".manage-workers-leave");
 const dashboardSearch = document.querySelector(".dashboard-search");
 const mainBody = document.querySelector(".main-body");
+const filterButton = document.querySelector(".filter-button");
+const filterContainer = document.querySelector(".filter-container");
 
 const fetchWorkers = async () => {
   // Getting the actual workers from the company
@@ -11,6 +13,7 @@ const fetchWorkers = async () => {
     const { companyName: workerCompanyName } = worker;
     return workerCompanyName.toLowerCase() === companyName.toLowerCase();
   });
+  console.log(companyWorkers);
 
   // ----------------------- search Function ------------------------------------//
 
@@ -21,7 +24,7 @@ const fetchWorkers = async () => {
       return firstName.toLowerCase().startsWith(userInput);
     });
     if (filtered.length == 0) {
-      manageWorkersSection.innerHTML = `No search results found for ${dashboardSearch.value}`;
+      manageWorkersSection.innerHTML = `No search results found for "${dashboardSearch.value}"`;
     } else {
       manageWorkersSection.innerHTML = "";
       filtered.map((data, index) => {
@@ -62,6 +65,8 @@ const fetchWorkers = async () => {
       });
     }
   };
+
+  // ------------------------ ADDING WORKERS ON DASHBOARD -----------------------//
 
   dashboardSearch.addEventListener("input", searchFunc);
   manageWorkersSection.innerHTML = "";
@@ -106,21 +111,253 @@ const fetchWorkers = async () => {
         `;
     manageWorkersSection.innerHTML += ihtml;
   });
+
+  // ----------------------------- SORTING ----------------------------------------//
+
+  // ----------------------------- SORTING BY ASCENDING NAME -------------------//
+
+  nameAsc.addEventListener("click", () => {
+    
+    filterContainer.classList.remove("filter-container-active");
+    icon.classList.remove("fa-circle-xmark");
+    
+    if (companyWorkers.length == 0) {
+      manageWorkersSection.innerHTML =
+        '<p class="empty-workers">You have no any workers to filter. Add workers to filter out</p>';
+    }
+    if (companyWorkers.length > 0) {
+      let nameAscending = companyWorkers.sort((a, b) => {
+        if (a.firstName.toLowerCase() < b.firstName.toLowerCase()) {
+          return -1;
+        }
+        if (a.firstName.toLowerCase() > b.firstName.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      });
+      manageWorkersSection.innerHTML = "";
+      nameAscending.map((data) => {
+        const { firstName, lastName, workerID, gender, email } = data;
+        const fullName = `${firstName} ${lastName}`;
+        let ihtml = `
+        <div class="worker-details">
+
+            <div class="topsection">
+            
+                <span>
+                    <i class="fa-regular fa-user user-icon"></i>
+                    <span class="worker-name">${fullName}</span>
+                </span>
+
+                <a href="#" class="worker-profile">
+                    <i class="fa-solid fa-ellipsis"></i>
+                </a>
+
+            </div>
+
+
+            <div class="worker-id">
+                <span>Worker ID: ${workerID}</span>
+            </div>
+
+            <div class="worker-gender-delete">
+                <p class="gender">${gender}</p>
+
+                <button class="worker-delete-button" onClick='confirmDelete(${workerID})'>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+
+        </div>
+        `;
+        manageWorkersSection.innerHTML += ihtml;
+      });
+    }
+  });
+
+  // ----------------------------- SORTING BY DESCENDING NAME -------------------//
+
+  nameDes.addEventListener("click", () => {
+
+    filterContainer.classList.remove("filter-container-active");
+    icon.classList.remove("fa-circle-xmark");
+
+    if (companyWorkers.length == 0) {
+      manageWorkersSection.innerHTML =
+        '<p class="empty-workers">You have no any workers to filter. Add workers to filter out</p>';
+    }
+
+    if (companyWorkers.length > 0) {
+      let nameDescending = companyWorkers.sort((a, b) => {
+        if (a.firstName.toLowerCase() < b.firstName.toLowerCase()) {
+          return 1;
+        }
+        if (a.firstName.toLowerCase() > b.firstName.toLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+      manageWorkersSection.innerHTML = "";
+      nameDescending.map((data) => {
+        const { firstName, lastName, workerID, gender, email } = data;
+        const fullName = `${firstName} ${lastName}`;
+        let ihtml = `
+        <div class="worker-details">
+
+            <div class="topsection">
+            
+                <span>
+                    <i class="fa-regular fa-user user-icon"></i>
+                    <span class="worker-name">${fullName}</span>
+                </span>
+
+                <a href="#" class="worker-profile">
+                    <i class="fa-solid fa-ellipsis"></i>
+                </a>
+
+            </div>
+
+
+            <div class="worker-id">
+                <span>Worker ID: ${workerID}</span>
+            </div>
+
+            <div class="worker-gender-delete">
+                <p class="gender">${gender}</p>
+
+                <button class="worker-delete-button" onClick='confirmDelete(${workerID})'>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+
+        </div>
+        `;
+        manageWorkersSection.innerHTML += ihtml;
+      });
+    }
+  });
+
+  // ----------------------------- SORTING BY ASCENDING ID -------------------//
+
+  idAsc.addEventListener("click", () => {
+
+    filterContainer.classList.remove("filter-container-active");
+    icon.classList.remove("fa-circle-xmark");
+
+    if (companyWorkers.length == 0) {
+      manageWorkersSection.innerHTML =
+        '<p class="empty-workers">You have no any workers to filter. Add workers to filter out</p>';
+    }
+    if (companyWorkers.length > 0) {
+      let idAscending = companyWorkers.sort((a, b) => {
+        return a.workerID - b.workerID;
+      });
+      manageWorkersSection.innerHTML = "";
+      idAscending.map((data) => {
+        const { firstName, lastName, workerID, gender, email } = data;
+        const fullName = `${firstName} ${lastName}`;
+        let ihtml = `
+        <div class="worker-details">
+
+            <div class="topsection">
+            
+                <span>
+                    <i class="fa-regular fa-user user-icon"></i>
+                    <span class="worker-name">${fullName}</span>
+                </span>
+
+                <a href="#" class="worker-profile">
+                    <i class="fa-solid fa-ellipsis"></i>
+                </a>
+
+            </div>
+
+
+            <div class="worker-id">
+                <span>Worker ID: ${workerID}</span>
+            </div>
+
+            <div class="worker-gender-delete">
+                <p class="gender">${gender}</p>
+
+                <button class="worker-delete-button" onClick='confirmDelete(${workerID})'>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+
+        </div>
+        `;
+        manageWorkersSection.innerHTML += ihtml;
+      });
+    }
+  });
+
+  // ----------------------------- SORTING BY DESCENDING ID -------------------//
+
+  idDes.addEventListener("click", () => {
+
+    filterContainer.classList.remove("filter-container-active");
+    icon.classList.remove("fa-circle-xmark");
+
+    if (companyWorkers.length == 0) {
+      manageWorkersSection.innerHTML =
+        '<p class="empty-workers">You have no any workers to filter. Add workers to filter out</p>';
+    }
+
+    if (companyWorkers.length > 0) {
+      let idDescending = companyWorkers.sort((a, b) => {
+        return b.workerID - a.workerID;
+      });
+      manageWorkersSection.innerHTML = "";
+      idDescending.map((data) => {
+        const { firstName, lastName, workerID, gender, email } = data;
+        const fullName = `${firstName} ${lastName}`;
+        let ihtml = `
+        <div class="worker-details">
+
+            <div class="topsection">
+            
+                <span>
+                    <i class="fa-regular fa-user user-icon"></i>
+                    <span class="worker-name">${fullName}</span>
+                </span>
+
+                <a href="#" class="worker-profile">
+                    <i class="fa-solid fa-ellipsis"></i>
+                </a>
+
+            </div>
+
+
+            <div class="worker-id">
+                <span>Worker ID: ${workerID}</span>
+            </div>
+
+            <div class="worker-gender-delete">
+                <p class="gender">${gender}</p>
+
+                <button class="worker-delete-button" onClick='confirmDelete(${workerID})'>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+
+        </div>
+        `;
+        manageWorkersSection.innerHTML += ihtml;
+      });
+    }
+  });
 };
 
 fetchWorkers();
 
 const confirmDelete = (deleteWorkerID) => {
-
   const confirmBox = document.querySelector(".confirm-container");
   const cancelButton = document.querySelector(".cancel-button");
   const confirmButton = document.querySelector(".confirm-button");
-  
+
   confirmBox.style.display = "flex";
   mainBody.classList.add("main-body-overflow");
-  
-  
-
 
   cancelButton.addEventListener("click", () => {
     confirmBox.style.display = "none";
@@ -128,10 +365,24 @@ const confirmDelete = (deleteWorkerID) => {
     console.log("you cancelled");
   });
   confirmButton.addEventListener("click", () => {
-    const parsedID = `"${deleteWorkerID}"`
+    const parsedID = `"${deleteWorkerID}"`;
     console.log(`deleted worker ID ${parsedID}`);
     deleteWorkerID = null;
     confirmBox.style.display = "none";
     mainBody.classList.remove("main-body-overflow");
   });
 };
+
+// ---------- filter ------- //
+
+const filteroptions = document.querySelectorAll(".filter-buttons");
+const nameAsc = document.querySelector(".asc-name");
+const nameDes = document.querySelector(".des-name");
+const idAsc = document.querySelector(".asc-id");
+const idDes = document.querySelector(".des-id");
+const icon = document.querySelector(".filter-icon");
+
+filterButton.addEventListener("click", () => {
+  filterContainer.classList.toggle("filter-container-active");
+  icon.classList.toggle("fa-circle-xmark");
+});
