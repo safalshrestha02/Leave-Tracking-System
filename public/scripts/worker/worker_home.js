b56a52b2d1d3d2abb0e5ae96fa1498dea1989470
FeNriR0077 <shardul.gautam123@gmail.com>
2023-02-27 17:14:46 +0545

    feat: added pending and active leaves on worker home

--- public/scripts/worker/worker_home.js
+++ public/scripts/worker/worker_home.js
@@ -5,13 +5,15 @@ const leavesRemaining = document.querySelector(".leaves-remaining");
 
 const activeLeavesContainer = document.querySelector(".active-leave-container");
 const activeLeavesDetails = document.querySelector(".active-leave-details");
-const pendingApprovedLeaves = document.querySelector(".pending-and-approved");
+const pendingApprovedLeaves = document.querySelector(".pending-and-approved-container");
+const pendingAndApproved = document.querySelector(".pending-and-approved");
+
 
 
 // Leave days panel 
 const fetchTotalLeaves = async () => {
     const totalLeaves = await fetchLeavesUnderWorker();
-    // console.log(totalLeaves.leaveHistory)
+    console.log(totalLeaves)
     leavesTaken.textContent = totalLeaves.leaveHistory.length
     leavesRemaining.textContent = parseInt(totalLeaveDays.textContent) - parseInt(leavesTaken.textContent);}
 fetchTotalLeaves()
@@ -56,7 +58,6 @@ const activeLeavesDisplay = async () => {
 
     if (activeLeaves.length !== 0) {
         activeLeaves.forEach((leave) => {
-            console.log(leave)
             let ihtml = `
                 <p class="leave-type">${leave.typeOfLeave}</p>
                 <div class="active-leave-date">
@@ -73,8 +74,11 @@ const activeLeavesDisplay = async () => {
             activeLeavesDetails.innerHTML += ihtml
         })
     } else {
-        console.log("noactive")
-        activeLeavesContainer.innerHTML = `<p class="display-none">No active leaves<p>`;
+        // console.log("noactive")
+        activeLeavesContainer.innerHTML = `
+        <h2>Active Leave</h2>
+        <p class="display-none">No active leave<p>
+        `;
     }
 }
 
@@ -86,8 +90,57 @@ activeLeavesDisplay();
 const pendingApprovedLeavesDisplay = async () => {
     const fetchTotalLeaves = await fetchLeavesUnderWorker();
     const totalLeaves = fetchTotalLeaves.leaveHistory;
-
+    // console.log(totalLeaves)
     
+    const pendingLeaves = totalLeaves.filter((pendingLeave) => {
+        // console.log(pendingLeave)
+        return pendingLeave.approveState === "pending"
+    });
+    
+    const approvedLeaves = totalLeaves.filter((approvedLeave) => {
+        return approvedLeave.approveState === "approved"
+    });
+
+    // sorting according to start date
+    const totalPendingLeaves = pendingLeaves.sort((req1, req2) => {
+        const {startDate: startDate1} = req1 
+        const {startDate: startDate2} = req2
+        return startDate1.slice(0,10) > startDate2.slice(0,10) ? -1 : startDate1.slice(0,10) < startDate2.slice(0,10) ? 1 : 0; 
+    });
+    const totalApprovedLeaves = approvedLeaves.sort((req1, req2) => {
+        const {startDate: startDate1} = req1 
+        const {startDate: startDate2} = req2
+        return startDate1.slice(0,10) > startDate2.slice(0,10) ? -1 : startDate1.slice(0,10) < startDate2.slice(0,10) ? 1 : 0; 
+    });
+
+    const totalPendingApprovedLeaves = [...totalPendingLeaves,...totalApprovedLeaves];
+
+
+    // ------------ mapping pending.approved leaves
+    if (totalPendingApprovedLeaves !== 0) {
+        totalPendingApprovedLeaves.forEach((leave) => {
+            let ihtml = `
+            <div class="worker-leaves">
+                <p class="leave-type">${leave.typeOfLeave}</p>
+                <div class="leave-date">
+                    <span>${leave.startDate
+                    .slice(0, 10)
+                    .replaceAll("-", "/")} - ${leave.endDate
+                      .slice(0, 10)
+                      .replaceAll("-", "/")}</span> <span> ${leave.leaveDays} days</span>
+                </div>
+                <p><i class="fa-solid fa-circle orange"></i>${leave.approveState}</p>
+            </div>
+            `
+            pendingApprovedLeaves.innerHTML += ihtml
+        })
+    } else {
+        pendingAndApproved.innerHTML = `
+        <h2>Pending/Approved Leaves</h2>
+        <p class="display-none no-pending-approved">No pending or approved leaves<p>
+        `
+    };
+
 };
 
 pendingApprovedLeavesDisplay();
