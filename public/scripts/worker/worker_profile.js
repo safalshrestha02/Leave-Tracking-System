df35d11cc72244fe7b8208d367bba68a04761208
Trishan Wagle <mailtotrishan@gmail.com>
2023-02-27 14:06:40 +0545

    feat: added company name to worker profile page.

--- public/scripts/worker/worker_profile.js
+++ public/scripts/worker/worker_profile.js
@@ -1,17 +1,21 @@
 const dashboardWorkerName = document.querySelector(".worker-name-profile");
+const dashboardCompany = document.querySelector('.emp-companyName-value');
 const dashboardWorkerID = document.querySelector(".emp-id-value");
 const dashboardWorkerEmail = document.querySelector(".emp-email-value");
 const dashboardWorkerGender = document.querySelector(".emp-gender-value");
 const dashboardWorkerCountry = document.querySelector(".emp-country-value");
 const dashboardWorkerCity = document.querySelector(".emp-city-value");
+const companyNameField = document.querySelector('#company-name')
 
 const fetchWorkerProfile = async () => {
   const { fullName, workerID, email, gender, country, city, companyName } = await fetchActiveWorkerApi();
   dashboardWorkerName.innerHTML = fullName;
+  dashboardCompany.innerHTML = companyName;
   dashboardWorkerID.innerHTML = workerID;
   dashboardWorkerEmail.innerHTML = email;
   dashboardWorkerGender.innerHTML = gender;
   dashboardWorkerCountry.innerHTML = country;
   dashboardWorkerCity.innerHTML = city;
+  companyNameField.innerHTML = companyName;
 };
 fetchWorkerProfile();
