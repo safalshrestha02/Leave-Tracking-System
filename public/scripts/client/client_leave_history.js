924bab89a8820386d8bce25b0f8962fbf75cf5cb
Trishan Wagle <mailtotrishan@gmail.com>
2023-03-02 15:20:16 +0545

    fix: fixed client leave history page.

--- public/scripts/client/client_leave_history.js
+++ public/scripts/client/client_leave_history.js
@@ -1,7 +1,17 @@
 const chooseWorker = document.querySelector('.select-worker')
 const leaveHistoryContainer = document.querySelector('.client-leave-history-container')
+
+const upcomingLeaves = document.querySelector('.upcoming-leave-history')
+const pastLeaves = document.querySelector('.past-leave-history')
+const rejectedLeavesMain = document.querySelector('.rejected-leave-history')
+
+const rejectedLeaveContainer = document.querySelector('.rejected-leave-history-container')
+const upcomingLeaveContainer = document.querySelector('.upcoming-leave-history-container')
+const pastLeaveContainer = document.querySelector('.past-leave-history-container')
+
 const filterButton = document.querySelector('.filter-button')
 const filterContainer = document.querySelector('.filter-container')
+let fetchLeaveHistory
 
 
 // Checking if client have any workers or not
@@ -38,6 +48,10 @@ const leaveHistory = async () => {
 
     // getting approved, rejected leaves of the worker selected by the client
     const getLeaveHistory = async () => {
+        upcomingLeaves.classList.remove('show-div')
+        pastLeaves.classList.remove('show-div')
+        rejectedLeavesMain.classList.remove('show-div')
+
         leaveHistoryContainer.innerHTML = '<p class = "loader">Loading Leave History. Please Wait...</p>'
         const approvedLeaves = await approvedLeaveRequestsUnderClient()
         const rejectedLeaves = await rejectedLeaveRequestsUnderClient()
@@ -55,68 +69,133 @@ const leaveHistory = async () => {
             return userChoiceWorker == workerID
         })
 
-        const userChoiceApprovedLeaves = userChoiceLeaveHistory.filter((leave) => {
-            const { approveState } = leave
-            return approveState == 'approved'
-        })
-
         const userChoiceRejectedLeaves = userChoiceLeaveHistory.filter((leave) => {
             const { approveState } = leave
             return approveState == 'rejected'
         })
 
+        const userChoiceApprovedLeaves = userChoiceLeaveHistory.filter((leave) => {
+            const { approveState } = leave
+            return approveState == 'approved'
+        })
+
+        const upcomingApprovedLeaves = userChoiceApprovedLeaves.filter((leave) => {
+            const { endDate } = leave
+            const endDateObj = new Date(endDate)
+
+            const currentDate = new Date()
+            const difference = (endDateObj - currentDate) / (1000 * 60 * 60 * 24) + 1
+
+            return difference > 0
+        })
+
+
+        const pastApprovedLeaves = userChoiceApprovedLeaves.filter((leave) => {
+            const { endDate } = leave
+            const endDateObj = new Date(endDate)
+
+            const currentDate = new Date()
+            const difference = (endDateObj - currentDate) / (1000 * 60 * 60 * 24) + 1
+
+
+            return difference < 0
+
+        })
+
+        // fetching leave history by mapping the data
+        fetchLeaveHistory = async () => {
+            upcomingLeaves.classList.remove('show-div')
+            pastLeaves.classList.remove('show-div')
+            rejectedLeavesMain.classList.remove('show-div')
+
+
+            if (upcomingApprovedLeaves.length > 0) {
+                upcomingLeaves.classList.add('show-div')
+                upcomingApprovedLeaves.forEach((leave) => {
+                    const { typeOfLeave, startDate, endDate, leaveDays, approveState } = leave
+                    const dayOrDays = leaveDays > 1 ? 'Days' : 'Day'
+
+                    let ihtml = `
+                        <div class="client-leave-history-details">
+        
+                            <p class="client-leave-history-leave-type">${typeOfLeave}</p>
+        
+                            <div class="client-leave-history-leave-date">
+                                <span>${startDate.slice(0, 10).replaceAll("-", "/")} - ${endDate.slice(0, 10).replaceAll("-", "/")}</span>
+                                <span> ${leaveDays} ${dayOrDays}</span>
+                            </div>
+        
+                            <div class="client-leave-history-leave-status">
+                                <p>
+                                    <i class="fa-solid fa-circle ${approveState}-leave"></i>
+                                    ${capitalize(approveState)}
+                                </p>
+                            </div>
+        
+                        </div>
+                                `
+                    leaveHistoryContainer.innerHTML = ''
+                    upcomingLeaveContainer.innerHTML = ''
+                    setTimeout(() => {
+                        upcomingLeaveContainer.insertAdjacentHTML('afterbegin', ihtml)
+                    })
+                })
+            }
+
+            if (pastApprovedLeaves.length > 0) {
+                pastLeaves.classList.add('show-div')
+                pastApprovedLeaves.forEach((leave) => {
+                    const { typeOfLeave, startDate, endDate, leaveDays, approveState } = leave
+                    const dayOrDays = leaveDays > 1 ? 'Days' : 'Day'
+
+                    let ihtml = `
+                            <div class="client-leave-history-details">
+    
+                                <p class="client-leave-history-leave-type">${typeOfLeave}</p>
+    
+                                <div class="client-leave-history-leave-date">
+                                    <span>${startDate.slice(0, 10).replaceAll("-", "/")} - ${endDate.slice(0, 10).replaceAll("-", "/")}</span>
+                                    <span> ${leaveDays} ${dayOrDays}</span>
+                                </div>
+    
+                                <div class="client-leave-history-leave-status">
+                                    <p>
+                                        <i class="fa-solid fa-circle ${approveState}-leave"></i>
+                                        ${capitalize(approveState)}
+                                    </p>
+                                </div>
+    
+                            </div>
+                                    `
+                    leaveHistoryContainer.innerHTML = ''
+                    pastLeaveContainer.innerHTML = ''
+                    setTimeout(() => {
+                        pastLeaveContainer.insertAdjacentHTML('afterbegin', ihtml)
+                    })
+                })
+            }
+        }
         if (userChoiceLeaveHistory.length == 0) {
+            checkWorkers()
             const blankArr = []
             leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
             return blankArr
         }
 
         else {
-            fetchLeaveHistory(userChoiceApprovedLeaves)
-            return { userChoiceLeaveHistory, userChoiceApprovedLeaves, userChoiceRejectedLeaves }
+            fetchLeaveHistory()
+            return { userChoiceLeaveHistory, userChoiceApprovedLeaves, upcomingApprovedLeaves, pastApprovedLeaves, userChoiceRejectedLeaves }
+
         }
     }
     chooseWorker.addEventListener('change', getLeaveHistory)
     window.addEventListener('load', getLeaveHistory)
 
 
-    // fetching leave history by mapping the data
-    const fetchLeaveHistory = async (data) => {
-        data.forEach((leave) => {
-            const { typeOfLeave, startDate, endDate, leaveDays, approveState } = leave
-            const dayOrDays = leaveDays > 1 ? 'Days' : 'Day'
-
-            let ihtml = `
-                <div class="client-leave-history-details">
-
-                    <p class="client-leave-history-leave-type">${typeOfLeave}</p>
-                
-                    <div class="client-leave-history-leave-date">
-                        <span>${startDate.slice(0, 10).replaceAll("-", "/")} - ${endDate.slice(0, 10).replaceAll("-", "/")}</span>
-                        <span> ${leaveDays} ${dayOrDays}</span>
-                    </div>
-                
-                    <div class="client-leave-history-leave-status">
-                        <p>
-                            <i class="fa-solid fa-circle ${approveState}-leave"></i>
-                            ${capitalize(approveState)}
-                        </p>
-                    </div>
-
-                </div>
-                        `
-            leaveHistoryContainer.innerHTML = ''
-            setTimeout(() => {
-                leaveHistoryContainer.insertAdjacentHTML('afterbegin', ihtml)
-            })
-        })
-    }
-
-
     // filter function
     const filterButtons = document.querySelectorAll('.filter-buttons')
-    const approvedLeaves = document.querySelector('.approved-past-leaves')
-    const rejectedLeaves = document.querySelector('.rejected-past-leaves')
+    const approvedLeavesFilter = document.querySelector('.approved-past-leaves')
+    const rejectedLeavesFilter = document.querySelector('.rejected-past-leaves')
     const icon = document.querySelector('.filter-icon')
 
     filterButton.addEventListener('click', () => {
@@ -125,7 +204,7 @@ const leaveHistory = async () => {
     })
 
     // getting approved leaves (filter)
-    approvedLeaves.addEventListener('click', async () => {
+    approvedLeavesFilter.addEventListener('click', async () => {
         try {
             filterContainer.classList.remove('filter-container-active')
             icon.classList.remove('fa-circle-xmark')
@@ -133,73 +212,79 @@ const leaveHistory = async () => {
             filterButtons.forEach((btn) => {
                 btn.classList.remove("active-option")
             })
-            approvedLeaves.classList.add("active-option")
-
-            const allLeaves = await getLeaveHistory()
-
-            if (allLeaves.length == 0) {
-                leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
-            }
-
-            else {
-                const { userChoiceApprovedLeaves } = allLeaves
-                leaveHistoryContainer.innerHTML = ''
-                setTimeout(() => {
-                    if (userChoiceApprovedLeaves.length > 0) {
-                        fetchLeaveHistory(userChoiceApprovedLeaves)
-                    }
-
-                    else if (userChoiceApprovedLeaves.length == 0) {
-                        leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any approved leaves records.</p>'
-                    }
-                })
-            }
+            approvedLeavesFilter.classList.add("active-option")
+            fetchLeaveHistory()
 
         }
 
         catch (err) {
-            leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
+            // leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
         }
 
     })
 
 
     // getting rejected leaves (filter)
-    rejectedLeaves.addEventListener('click', async () => {
-        try {
-            filterContainer.classList.remove('filter-container-active')
-            icon.classList.remove('fa-circle-xmark')
+    rejectedLeavesFilter.addEventListener('click', async () => {
 
-            filterButtons.forEach((btn) => {
-                btn.classList.remove("active-option")
-            })
-            rejectedLeaves.classList.add("active-option")
+        filterContainer.classList.remove('filter-container-active')
+        icon.classList.remove('fa-circle-xmark')
 
-            const allLeaves = await getLeaveHistory()
+        filterButtons.forEach((btn) => {
+            btn.classList.remove("active-option")
+        })
+        rejectedLeavesFilter.classList.add("active-option")
 
-            if (allLeaves.length == 0) {
-                leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
-            }
+        upcomingLeaves.classList.remove('show-div')
+        pastLeaves.classList.remove('show-div')
 
-            else {
-                const { userChoiceRejectedLeaves } = allLeaves
+
+        const rejectedLeaves = await rejectedLeaveRequestsUnderClient()
+        const sortedRejectedLeaves = rejectedLeaves.sort((leaves1, leaves2) => {
+            const { startDate: startDate1 } = leaves1
+            const { startDate: startDate2 } = leaves2
+            return startDate1.slice(0, 10) > startDate2.slice(0, 10) ? 1 : startDate1.slice(0, 10) < startDate2.slice(0, 10) ? -1 : 0
+        })
+
+        const userChoiceWorker = chooseWorker.value
+        const userChoiceRejectedLeaves = sortedRejectedLeaves.filter((leave) => {
+            const { workerID } = leave
+            return userChoiceWorker == workerID
+        })
+
+        if (userChoiceRejectedLeaves.length > 0) {
+            rejectedLeavesMain.classList.add('show-div')
+            userChoiceRejectedLeaves.forEach((leave) => {
+                const { typeOfLeave, startDate, endDate, leaveDays, approveState } = leave
+                const dayOrDays = leaveDays > 1 ? 'Days' : 'Day'
+
+                let ihtml = `
+                        <div class="client-leave-history-details">
+    
+                            <p class="client-leave-history-leave-type">${typeOfLeave}</p>
+    
+                            <div class="client-leave-history-leave-date">
+                                <span>${startDate.slice(0, 10).replaceAll("-", "/")} - ${endDate.slice(0, 10).replaceAll("-", "/")}</span>
+                                <span> ${leaveDays} ${dayOrDays}</span>
+                            </div>
+    
+                            <div class="client-leave-history-leave-status">
+                                <p>
+                                    <i class="fa-solid fa-circle ${approveState}-leave"></i>
+                                    ${capitalize(approveState)}
+                                </p>
+                            </div>
+    
+                        </div>
+                                `
                 leaveHistoryContainer.innerHTML = ''
+                rejectedLeaveContainer.innerHTML = ''
                 setTimeout(() => {
-                    if (userChoiceRejectedLeaves.length > 0) {
-                        fetchLeaveHistory(userChoiceRejectedLeaves)
-                    }
-
-                    else if (userChoiceRejectedLeaves.length == 0) {
-                        leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any rejected leaves records.</p>'
-                    }
+                    rejectedLeaveContainer.insertAdjacentHTML('afterbegin', ihtml)
                 })
-            }
-
+            })
         }
 
-        catch (err) {
-            leaveHistoryContainer.innerHTML = '<p class = "no-past-leaves">The requested user has no any leaves records.</p>'
-        }
 
     })
 
