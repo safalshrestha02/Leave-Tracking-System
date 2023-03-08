bfea5aa3db39a386ec7c34ee41d3866eff78545f
ishanghimire11 <ishanuchiha11@gmail.com>
2023-03-08 16:22:34 +0545

    feat: change password design

--- public/scripts/client/client_profile.js
+++ public/scripts/client/client_profile.js
@@ -5,14 +5,14 @@ const dashboardCompanyName = document.querySelector(".client-company-value");
 const dashboardCompanyAddress = document.querySelector(".client-company_address-value");
 const dashboardCompanyID = document.querySelector(".client-companyID-value");
 const dashboardWorkers = document.querySelector(".client-workers-value");
-const companyNameFieldProfile = document.querySelector('#company-name');
-
+const companyNameFieldProfile = document.querySelector("#company-name");
 
 const fetchClientProfile = async () => {
   try {
-    const totalWorkers = await workersUnderClient()
-    const { workers } = totalWorkers
-    const { companyName, companyAddress, clientName, clientEmail, companyID } = await fetchActiveClientApi();
+    const totalWorkers = await workersUnderClient();
+    const { workers } = totalWorkers;
+    const { companyName, companyAddress, clientName, clientEmail, companyID } =
+      await fetchActiveClientApi();
     dashboardClientName.innerHTML = clientName;
     dashboardClientEmail.innerHTML = clientEmail;
     dashboardCompanyName.innerHTML = companyName;
@@ -25,3 +25,17 @@ const fetchClientProfile = async () => {
   }
 };
 fetchClientProfile();
+
+// --------------------------------------- CHANGE PASSWORD ------------------------------- //
+
+const passwordContainer = document.querySelector(".main-password-change-container");
+const changePasswordButton = document.querySelector(".change-password-button");
+const cancelButton = document.querySelector(".cancel-button");
+
+changePasswordButton.addEventListener("click", () => {
+  passwordContainer.classList.add("change-password-active");
+});
+
+cancelButton.addEventListener("click", () => {
+  passwordContainer.classList.remove("change-password-active");
+});
