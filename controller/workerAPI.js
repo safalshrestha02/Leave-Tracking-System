1e4d8b48680e4b757d6a1fec8495495c0e579b60
Safal <Safal Shrestha>
2023-03-09 15:51:11 +0545

    fix: cleaned codes

--- controller/workerAPI.js
+++ controller/workerAPI.js
@@ -1,47 +1,37 @@
-const Worker = require("../models/Worker");
-const Messages = require("../models/Leave");
+const Leaves = require("../models/Leave");
 
 exports.leaveRequestDelete = async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    const deleting = await Messages.findByIdAndDelete(id);
+    await Leaves.findByIdAndDelete(id);
     res.status(201).json({ "successfully Deleted": id });
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 
 exports.workersLeaves = async (req, res, next) => {
   const { page = 1, limit = 1000 } = req.query;
-  const count = await Messages.count();
+  const count = await Leaves.count();
   const { id } = req.params;
 
   try {
-    const worker = await Worker.findById(id).then((result) => {
-      if (result) {
-        const leave = Messages.find({ "workerDetails._id": id })
-          .limit(limit * 1)
-          .skip((page - 1) * limit)
-          .then((leaves) => {
-            if (leaves) {
-              res.status(201).json({
-                worker: result,
-                leaveHistory: leaves,
-                totalPages: Math.ceil(count / limit),
-                currentPage: page,
-              });
-            } else {
-              res
-                .status(400)
-                .json({ error: "No leave requests from that Worker" });
-            }
+    await Leaves.find({ "workerDetails._id": id })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .then((leaves) => {
+        if (leaves) {
+          res.status(201).json({
+            leaveHistory: leaves,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
           });
-      } else {
-        res.status(400).json({ error: "No worker under that ID" });
-      }
-    });
+        } else {
+          res.status(400).json({ error: "No worker under that ID" });
+        }
+      });
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
